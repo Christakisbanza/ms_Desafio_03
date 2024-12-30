e131930c90320bb1173a67b4162efa88d34847b0
Christakis <banzachristakis@gmail.com>
2024-12-30 15:31:39 -0300

    feat: configuração do Rabbitmq implementado

---
+++ ms2Desafio03/ms2Desafio03/src/main/java/com/ms2Desafio03/ms2Desafio03/config/RabbitmqConfig.java
@@ -0,0 +1,18 @@
+package com.ms2Desafio03.ms2Desafio03.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Value("${spring.rabbitmq.queue}")
+    private String queue;
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queue,true);
+    }
+}
